cf886a2206e6a44c3b1915631e1eb08e12b04214
Naymul Islam <naymul504@gmail.com>
2023-11-03 12:39:32 +0600

    add turtles of the pabble, ball and bricks

---
+++ main.py
@@ -0,0 +1,14 @@
+import turtle
+
+window = turtle.Screen()
+window.title("BreakoutBlast: Brick Buster Challenge")
+
+
+
+paddle = turtle.Turtle()
+ball = turtle.Turtle()
+bricks = turtle.Turtle()
+
+
+
+window.mainloop()
